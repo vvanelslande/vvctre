34973f4ec648a060ddfa25536c8979e4645def7a
Valentin Vanelslande <vvanelslandedev@gmail.com>
2020-07-01 17:10:51 -0500

    Remove unused includes

--- src/vvctre/emu_window/emu_window_sdl2.cpp
+++ src/vvctre/emu_window/emu_window_sdl2.cpp
@@ -43,7 +43,6 @@
 #include "input_common/sdl/sdl.h"
 #include "network/network.h"
 #include "video_core/renderer_base.h"
-#include "video_core/renderer_opengl/post_processing_opengl.h"
 #include "video_core/renderer_opengl/texture_filters/texture_filterer.h"
 #include "video_core/video_core.h"
 #include "vvctre/common.h"
--- src/vvctre/initial_settings.cpp
+++ src/vvctre/initial_settings.cpp
@@ -31,7 +31,6 @@
 #include "core/movie.h"
 #include "core/settings.h"
 #include "input_common/main.h"
-#include "video_core/renderer_opengl/post_processing_opengl.h"
 #include "video_core/renderer_opengl/texture_filters/texture_filterer.h"
 #include "vvctre/common.h"
 #include "vvctre/initial_settings.h"
