59bc216f5453c0bb06865a4dcdab34685517e62d
Valentin Vanelslande <vvanelslandedev@gmail.com>
2020-08-11 03:48:24 -0500

    Custom Default Settings Plugin Request: Add bot that removes useless lines

---
+++ .github/scripts/custom-default-settings-plugin-request-remove-useless-lines.js
@@ -0,0 +1,29 @@
+// Copyright 2020 Valentin Vanelslande
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+const fs = require("fs");
+const getRegexes = require("./common/regexes");
+
+module.exports = (github, context) => {
+  const linesAfterEdit = [];
+  const lines = context.issue.body.split("\r\n");
+  const regexes = getRegexes(null, null, null);
+
+  lines.forEach((line) => {
+    regexes.forEach((test) => {
+      if (test.regex.test(line)) {
+        linesAfterEdit.push(line);
+      }
+    });
+  });
+
+  if (linesAfterEdit.length > 0) {
+    github.issues.update({
+      issue_number: context.issue.number,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      body: linesAfterEdit.join("\r\n"),
+    });
+  }
+};
