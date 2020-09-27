531903ddf39a54ba1cd645bde91dd7a11a4d9b8a
Valentin Vanelslande <vvanelslandedev@gmail.com>
2020-09-27 17:56:51 -0500

    remove unused include

--- src/vvctre/camera/image.cpp
+++ src/vvctre/camera/image.cpp
@@ -2,7 +2,6 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
-#include <asl/Http.h>
 #include <curl/curl.h>
 #include <httpparser/urlparser.h>
 #include <mbedtls/ssl.h>
