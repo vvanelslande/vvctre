e83a91ab9634a819b499fc0fb21565a13f36bcee
Valentin Vanelslande <vvanelslandedev@gmail.com>
2020-10-01 10:06:35 -0500

    Change version to 38.0.6

--- src/vvctre/common.cpp
+++ src/vvctre/common.cpp
@@ -30,7 +30,7 @@
 
 const u8 vvctre_version_major = 38;
 const u8 vvctre_version_minor = 0;
-const u8 vvctre_version_patch = 5;
+const u8 vvctre_version_patch = 6;
 
 void vvctreShutdown(PluginManager* plugin_manager) {
     if (plugin_manager != nullptr) {
