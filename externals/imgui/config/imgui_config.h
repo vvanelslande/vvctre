dc218d03828fa97794ff70dde13072cb9272a04f
Valentin Vanelslande <vvanelslandedev@gmail.com>
2020-10-06 08:19:39 -0500

    Add IMGUI_DISABLE_{OBSOLETE_FUNCTIONS,DEMO_WINDOWS,METRICS_WINDOW} and move IMGUI_IMPL_OPENGL_LOADER_GLAD

---
+++ externals/imgui/config/imgui_config.h
@@ -0,0 +1,10 @@
+// Copyright 2020 vvctre project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
+#define IMGUI_DISABLE_DEMO_WINDOWS
+#define IMGUI_DISABLE_METRICS_WINDOW
+#define IMGUI_IMPL_OPENGL_LOADER_GLAD
