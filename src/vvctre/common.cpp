6b81adc7979889ba3c539b0d0e94e2c1a83ac37d
Valentin Vanelslande <vvanelslandedev@gmail.com>
2021-03-07 00:38:07 -0500

    Change `vvctre_version_patch` to `2`

--- src/vvctre/common.cpp
+++ src/vvctre/common.cpp
@@ -31,7 +31,7 @@
 
 const u8 vvctre_version_major = 47;
 const u8 vvctre_version_minor = 0;
-const u8 vvctre_version_patch = 1;
+const u8 vvctre_version_patch = 2;
 
 void vvctreShutdown(PluginManager* plugin_manager) {
     if (plugin_manager != nullptr) {
